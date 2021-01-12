3877755c06bad04cae06ede26206f6d68b963567
LumbayanGedeon <60123661+LumbayanGedeon@users.noreply.github.com>
2021-01-12 00:19:39 -0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/page/home_page.dart';
+import 'package:todo/provider/todos.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'TODO';
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => TodosProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: title,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            scaffoldBackgroundColor: Color(0xFFf6f5ee),
+          ),
+          home: HomePage(),  
+        ),
+      );
+}
